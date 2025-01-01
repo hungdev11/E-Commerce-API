1892016da231b2864820e5c7bb00d868268b4524
HUNG <hungphangialai11@gmail.com>
2025-01-01 14:39:59 +0700

    update: -handle exception product not found/exist, normalization error code for application -error, page response

--- src/main/java/vn/pph/OrderManagementSysAPI/base/GlobalExceptionHandler.java
+++
@@ -1,4 +0,0 @@
-package vn.pph.OrderManagementSysAPI.base;
-
-public class GlobalExceptionHandler {
-}
---
+++ src/main/java/vn/pph/OrderManagementSysAPI/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package vn.pph.OrderManagementSysAPI.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ErrorResponse {
+    LocalDateTime timestamp;
+    int status;
+    String path;
+    String error;
+    String message;
+}
